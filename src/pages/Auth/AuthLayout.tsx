dcd3db2f0159b854a864f5e53e67d37b06e63ec8
sergromm <sergichev@gmail.com>
2023-10-02 05:57:08 +0300

    change selected route logic on auth page

--- src/pages/Auth/AuthLayout.tsx
+++ src/pages/Auth/AuthLayout.tsx
@@ -1,28 +1,18 @@
 import { motion } from 'framer-motion';
 import * as React from 'react';
-import { NavLink, Outlet } from 'react-router-dom';
+import { NavLink, Outlet, useLocation } from 'react-router-dom';
 import vidoe from 'assets/a8798f8d21b54ecbaac541576ca06bef.mp4';
 import image from 'assets/hero-image-placeholder.jpg';
 import { Text } from 'components/Text';
 import { routerPaths } from 'config/routerPaths';
 import styles from './Auth.module.scss';
 
-type NavClass = {
-  isActive: boolean;
-  isPending: boolean;
-};
-
 const Navigation: React.FC<React.PropsWithChildren<{ to: string }>> = ({ to, children }) => {
-  const [isActive, setIsActive] = React.useState(false);
-
-  const setClass = ({ isActive }: NavClass) => {
-    setIsActive(isActive);
-    return styles.link;
-  };
+  const { pathname } = useLocation();
 
   return (
-    <NavLink className={setClass} to={to}>
-      <Text color={isActive ? 'primary' : 'secondary'} view="h-32" weight="bold">
+    <NavLink className={styles.link} to={to}>
+      <Text color={pathname === to ? 'primary' : 'secondary'} view="h-32" weight="bold">
         {children}
       </Text>
     </NavLink>
