45b8adbad0214b208afc7cb78551ebef3333fb9f
sergromm <sergichev@gmail.com>
2023-10-05 07:43:06 +0300

    add placeholder text to favorites page

--- src/pages/Profile/Favorites.tsx
+++ src/pages/Profile/Favorites.tsx
@@ -8,9 +8,15 @@ import styles from './Favorites.module.scss';
 export const Favorites: React.FC = observer(() => {
   return (
     <div className={styles.favorites}>
-      <Text view="title" weight="bold">
-        Your favorites
-      </Text>
+      {userStore.favorites.length > 0 ? (
+        <Text view="title" weight="bold">
+          Your favorites
+        </Text>
+      ) : (
+        <Text color="secondary" view="h-32" weight="bold">
+          You don&apos;t have any favorites, yet
+        </Text>
+      )}
       <FavoritesGrid favorites={userStore.favorites} />
     </div>
   );
